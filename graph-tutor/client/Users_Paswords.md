c7da4245c8b26ab37dc2f4f86434b4b3b40cb662
Dttrifonov <135382211+Dttrifonov@users.noreply.github.com>
2023-06-11 19:50:44 +0300

    Add files via upload

---
+++ graph-tutor/client/Users_Paswords.md
@@ -0,0 +1,8 @@
+boro@abv.bg
+12345
+
+gogo@abv.bg
+12345
+
+nely@abv.bg
+12345
---
+++ graph-tutor/config.js
@@ -0,0 +1,2 @@
+export const MONGO_URI = "mongodb+srv://boris:12345@cluster0.q9asopt.mongodb.net/graphqldb?retryWrites=true&w=majority"
+export const JWT_SECRET = "dsa"
